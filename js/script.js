3d7e6733b0bd6e450b532211baa2e26f9302b24b
Augusto Tobia <augustotobia@gmail.com>
2020-11-10 02:52:50 -0300

    added a high score list

--- js/script.js
+++ js/script.js
@@ -1,21 +1,31 @@
 'use strict';
 (function (window, undefined) {
-
-
+    //Canvas and buffer
     var gameWindow = undefined,
     gameCtx = undefined,
+    buffer = null,
+    bufferCtx = null,
+    bufferScale = 1,
+    bufferOffsetX = 0,
+    bufferOffsetY = 0,
+    //Hit boxes
+    body = [],
+    food = new rectangle(80, 80, 10, 10),
+    wall = [],
+    //Controls
     lastCommand = undefined,
     keyLeft = 37,
     keyUp = 38,
     keyRight = 39,
     keyDown = 40,
     dir = 1,
+    //Pause
     stop = true,
-    body = [],
-    food = new rectangle(80, 80, 10, 10),
-    wall = [],
+    gameOver = false,
+    //Scores
     score = 0,
-    gameOver = false;
+    highscores = [],
+    posHighscore = 10;
 
     //assets
     var iNeck = new Image(),
@@ -32,21 +42,79 @@
     frames = 0,
     acumDelta = 0;
         
-    //Buffer
-    var buffer = null,
-    bufferCtx = null,
-    bufferScale = 1,
-    bufferOffsetX = 0,
-    bufferOffsetY = 0;
-
     //Scenes
     var currentScene = 0,
-    scenes = []
+        scenes = [];
+    var mainScene = undefined,
+        gameScene = undefined,
+        scoresScene = undefined;
 
+    //Objets and prototypes
+    function scene() {
+        this.id = scenes.length;
+        scenes.push(this);
+    }
+    
+    scene.prototype = {
+        constructor: scene,
+        load: function () {},
+        paint: function (bufferCtx) {},
+        act: function () {}
+    };
+
+    function loadScene(scene) {
+        currentScene = scene.id;
+        scenes[currentScene].load();
+        }
+        
+    function rectangle (x, y, width, height){
+        this.x = (x === undefined) ? 0 : x;
+        this.y = (y === undefined) ? 0 : y;
+        this.width = (width === undefined) ? 0 : width;
+        this.height = (height === undefined) ? this.width : height;
+    }
+    rectangle.prototype = {
+        constructor: rectangle,
+            
+        intersect : function (rect) {
+            if (rect === undefined){
+                window.console.warn('Missing parameters on function intersect')
+            } else {
+                return (
+                    this.x < rect.x + rect.width &&
+                    this.x + this.width > rect.x &&
+                    this.y < rect.y + rect.height &&
+                    this.y + this.height > rect.y
+                )
+            }
+        },
+
+        fill : function (gameCtx) {
+            if (gameCtx === undefined) {
+            window.console.warn('Missing parameters on function fill');
+            } else {
+            gameCtx.fillRect(this.x, this.y, this.width, this.height);
+            }
+        },  
+
+        drawImage : function (gameCtx, img) {
+            if (img === undefined) {
+            window.console.warn('Missing parameters on function drawImage');
+            } else {
+                if (img.width) {
+                gameCtx.drawImage(img, this.x, this.y);
+                } else {
+                gameCtx.strokeRect(this.x, this.y, this.width, this.height);
+                }
+            }
+        }
+    }
+    
+    //Random
     function random(max){
         return ~~(Math.random() * max);
     }
-
+    //Request animation
     window.requestAnimationFrame = (function (){
         return window.requestAnimationFrame ||
         window.mozRequestAnimationFrame ||
@@ -56,54 +124,7 @@
         }
     }());
 
-    function rectangle (x, y, width, height){
-        this.x = (x === undefined) ? 0 : x;
-        this.y = (y === undefined) ? 0 : y;
-        this.width = (width === undefined) ? 0 : width;
-        this.height = (height === undefined) ? this.width : height;
-    }
-    rectangle.prototype.intersect = function (rect) {
-        if (rect === undefined){
-            window.console.warn('Missing parameters on function intersect')
-        } else {
-            return (
-                this.x < rect.x + rect.width &&
-                this.x + this.width > rect.x &&
-                this.y < rect.y + rect.height &&
-                this.y + this.height > rect.y
-            )
-        }
-    }
-
-    rectangle.prototype.fill = function (gameCtx) {
-        if (gameCtx === undefined) {
-        window.console.warn('Missing parameters on function fill');
-        } else {
-        gameCtx.fillRect(this.x, this.y, this.width, this.height);
-        }
-    }   
-
-    rectangle.prototype.drawImage = function (gameCtx, img) {
-        if (img === undefined) {
-        window.console.warn('Missing parameters on function drawImage');
-        } else {
-            if (img.width) {
-            gameCtx.drawImage(img, this.x, this.y);
-            } else {
-            gameCtx.strokeRect(this.x, this.y, this.width, this.height);
-            }
-        }
-    }
-    
     //Rezising the game window
-    // function resize(){
-    //     var w = window.innerWidth / gameWindow.width,
-    //         h = window.innerHeight / gameWindow.height,
-    //         scale = Math.min(h, w);
-    //         scale = scale * 0.9;
-    //     gameWindow.style.width = (gameWindow.width * scale) + 'px';
-    //     gameWindow.style.height = (gameWindow.height * scale) + 'px';
-    // }
     function resize() {
         gameWindow.width = window.innerWidth * 0.9;
         gameWindow.height = window.innerHeight * 0.9;
@@ -115,15 +136,48 @@
         bufferOffsetY = (gameWindow.height - (buffer.height * bufferScale)) / 2;
         }
 
-
-
-
-    //Comands and actions.
+    //Score list
+    function addHighscore(score) {
+        posHighscore = 0;
+        while (highscores[posHighscore] > score && posHighscore < highscores.length) {
+            posHighscore += 1;
+        }
+        highscores.splice(posHighscore, 0, score);
+        if (highscores.length > 10) {
+            console.log(highscores.length);
+            highscores.length = 10;
+        }
+        localStorage.highscores = highscores.join(',');
+    }
+    
+        
+    //Title card.
+    mainScene = new scene();
+    mainScene.paint = function(bufferCtx){
+        //Clean canvas
+        bufferCtx.fillStyle = '#030';
+        bufferCtx.fillRect(0, 0, buffer.width, buffer.height);
+        //Draw title
+        bufferCtx.fillStyle = '#fff';
+        bufferCtx.textAlign = 'center';
+        bufferCtx.fillText('SNAKE', 150, 60);
+        bufferCtx.fillText('Press Enter', 150, 90);
+    }
+    mainScene.act = function(){
+        // Load next scene
+        if (lastCommand === 13 || lastCommand === 'Enter'){
+            loadScene(gameScene);
+            lastCommand = null;
+        }
+    }
+    //Game scene
+    gameScene = new scene();
+    //Controls and comands
     document.addEventListener('keydown', function (event) {
         lastCommand = event.key || event.keyCode || event.code
-        }, false);
+    }, false);
 
-    function act(deltaTime) {
+    gameScene.act = function() {
         if (!stop){
 
             //worm Movement
@@ -159,20 +213,6 @@
                 body[0].x -= 10;
                 }
 
-            // if (dir === 0) {
-            //     body[0].y -= 120 * deltaTime;
-            //     }
-            // if (dir === 1) {
-            //     body[0].x += 120 * deltaTime;
-            //     }
-            // if (dir === 2) {
-            //     body[0].y += 120 * deltaTime;
-            //     }
-            // if (dir === 3) {
-            //     body[0].x -= 120 * deltaTime;
-            //     }
-
-            //Pass through walls   
             if (body[0].x > buffer.width) {
                 body[0].x = 0;
                 }
@@ -186,113 +226,148 @@
                 body[0].y = buffer.height;
                 }
             }
-            //Pause or Game Over
+            //Pause & Game Over
             if (lastCommand === 13 || lastCommand === 'Enter'){
                 stop = !stop;
                 if (gameOver){
-                    reset();
+                    loadScene(scoresScene);
                 }
                 lastCommand = null;
             }
-            //Objet Colision
+            //Hit box Colisions
             if (body[0].intersect(food)) {
-                body.push(new rectangle(0, 0, 10, 10));
-                score += 1;
-                aEat.play();
-                food.x = random(buffer.width / 10 - 1) * 10;
-                food.y = random(buffer.height / 10 - 1) * 10;
+                    body.push(new rectangle(0, 0, 10, 10));
+                    score += 1;
+                    aEat.play();
+                    food.x = random(buffer.width / 10 - 1) * 10;
+                    food.y = random(buffer.height / 10 - 1) * 10;
                 }
 
             for (var i = 0, l = wall.length; i < l; i += 1) {
                 if (food.intersect(wall[i])) {
-                food.x = random(buffer.width / 10 - 1) * 10;
-                food.y = random(buffer.height / 10 - 1) * 10;
+                    food.x = random(buffer.width / 10 - 1) * 10;
+                    food.y = random(buffer.height / 10 - 1) * 10;
                 }
                 if (body[0].intersect(wall[i])) {
-                aDie.play();
-                stop = true;
-                gameOver = true; 
+                    aDie.play();
+                    stop = true;
+                    if(gameOver == false)
+                    addHighscore(score);
+                    gameOver = true; 
                 }
             }
             for (var i = 2; i < body.length; i += 1) {
                 if (body[0].intersect(body[i])) {
-                aDie.play();
-                gameOver = true;
-                stop = true;
+                    aDie.play();
+                    gameOver = true;
+                    stop = true;
+                    addHighscore(score);
                 }
             }
             
     }
 
-        //Rendering of the play screen
-        function paint(bufferCtx) {
-            //Clean canvas
-            bufferCtx.fillStyle = '#000';
-            bufferCtx.fillRect(0, 0, gameWindow.width, gameWindow.height);
-        
-            //Draw snake
-            for (var i = 0; i < body.length; i++){
-                if (i === 0){
-                    bufferCtx.fillStyle = '#E7D084'
-                    body[i].fill(bufferCtx);
-                } else if(i === 1) {
-                    body[i].drawImage(bufferCtx, iNeck);
-                    // ctx.drawImage(iNeck, body[i].x, body[i].y);
-                } else if (i < body.length){
-                    body[i].drawImage(bufferCtx, iBody);
-                    // ctx.drawImage(iBody, body[i].x, body[i].y);
-                } else {
-                    body[i].drawImage(bufferCtx, iTale);
-                    // ctx.drawImage(iTale, body[i].x, body[i].y);
-                }
-            }
-            // Draw apple
-            food.drawImage(bufferCtx, iApple);
-            bufferCtx.fillStyle = '#fff'
-
-            //Draw walls
-            bufferCtx.fillStyle = '#066'
-            for (i = 0; i < wall.length; i++){
-                wall[i].fill(bufferCtx);
-            }
-
-            //Draw Score
-            bufferCtx.fillText('Score: ' + score, 10, 10);
-            
-            //Draw FPS
-            bufferCtx.fillText('FPS: ' + FPS, 250, 10);
-
-            //Draw pause || game over
-            bufferCtx.fillStyle = '#fff'
-            if (stop) {
-                bufferCtx.textAlign = 'center';
-                if (gameOver) {
-                    bufferCtx.fillText('GAME OVER', 150, 75);
-                    bufferCtx.fillText('Press ENTER to restart', 150, 95);
-                } else {
-                    bufferCtx.fillText('PAUSE', 150, 75);
-                }
-                bufferCtx.textAlign = 'left';
-                }
-        }
+    //Rendering of the playable screen
+    gameScene.paint = function(bufferCtx){
+        //Clean canvas
+        bufferCtx.fillStyle = '#000';
+        bufferCtx.fillRect(0, 0, gameWindow.width, gameWindow.height);
     
+        //Draw snake
+        for (var i = 0; i < body.length; i++){
+            if (i === 0){
+                bufferCtx.fillStyle = '#E7D084'
+                body[i].fill(bufferCtx);
+            } else if(i === 1) {
+                body[i].drawImage(bufferCtx, iNeck);
+            } else if (i < body.length -1){
+                body[i].drawImage(bufferCtx, iBody);
+            } else {
+                body[i].drawImage(bufferCtx, iTale);
+            }
+        }
+        // Draw apple
+        food.drawImage(bufferCtx, iApple);
+        bufferCtx.fillStyle = '#fff'
+
+        //Draw obstacle
+        bufferCtx.fillStyle = '#066'
+        for (i = 0; i < wall.length; i++){
+            wall[i].fill(bufferCtx);
+        }
+
+        //Draw Score
+        bufferCtx.fillText('Score: ' + score, 50, 10);
+        
+        //Draw FPS
+        bufferCtx.fillText('FPS: ' + FPS, 250, 10);
+
+        //Draw pause
+        bufferCtx.fillStyle = '#fff'
+        if (stop) {
+            bufferCtx.textAlign = 'center';
+            if (gameOver) {
+                bufferCtx.fillText('GAME OVER', 150, 75);
+                bufferCtx.fillText('Press ENTER to restart', 150, 95);
+            } else {
+                bufferCtx.fillText('PAUSE', 150, 75);
+            }
+        }
+    }
+    //Scores scene
+    scoresScene = new scene();
+    scoresScene.paint = function(bufferCtx){
+        var i = 0,
+            l = 0;
+        // Clean canvas
+        bufferCtx.fillStyle = '#030';
+        bufferCtx.fillRect(0, 0, buffer.width, buffer.height);
+        // Draw title
+        bufferCtx.fillStyle = '#fff';
+        bufferCtx.textAlign = 'center';
+        bufferCtx.fillText('HIGH SCORES', 150, 30);
+        // Draw high scores
+        bufferCtx.textAlign = 'right';
+        for (i = 0, l = highscores.length; i < l; i += 1) {
+            if (i === posHighscore) {
+                bufferCtx.fillText('Your score: ' + highscores[i], 180, 40 + i * 10);
+            } else {
+                bufferCtx.fillText(highscores[i], 180, 40 + i * 10);
+            }
+        }
+    }
+    scoresScene.act = function () {
+        // Load next scene
+        if (lastCommand === 13 || lastCommand === 'Enter'){
+            loadScene(gameScene);
+            lastCommand = undefined;
+        }
+    };
+        
+    //Game Animation
     function repaint() {
-            //Fill buffer
-            window.requestAnimationFrame(repaint);
-            paint(bufferCtx);
+        //Render buffer frame
+        window.requestAnimationFrame(repaint);
+            
+        if (scenes.length) {
+            scenes[currentScene].paint(bufferCtx);
+
             //Clean game Window
             gameCtx.fillStyle = '#000';
             gameCtx.fillRect(0, 0, gameWindow.width, gameWindow.height);
-            //Draw buffer into game window
+            //Draw buffer into game frame
             gameCtx.imageSmoothingEnabled = false;
             gameCtx.webkitImageSmoothingEnabled = false;
             gameCtx.mozImageSmoothingEnabled = false;
             gameCtx.msImageSmoothingEnabled = false;
             gameCtx.oImageSmoothingEnabled = false;
-            gameCtx.drawImage(buffer, bufferOffsetX, bufferOffsetY, buffer.width * bufferScale, buffer.height * bufferScale)    }
+            gameCtx.drawImage(buffer, bufferOffsetX, bufferOffsetY, buffer.width * bufferScale, buffer.height * bufferScale);
+        }
+    }
     
     function run() {
-        setTimeout(run, 25)
+        setTimeout(run, 25);
+        //Get FPS
         var now = Date.now(),
             deltaTime = (now - lastUpdate) / 1000;
         if (deltaTime > 1) {
@@ -307,12 +382,14 @@
             frames = 0;
             acumDelta -= 1;
         }
-              
-        act(deltaTime);
+         //Run game     
+        if (scenes.length) {
+            scenes[currentScene].act();
+        }
     }
 
     //Restart the game
-    function reset() {
+    gameScene.load = function() {
         score = 0;
         dir = 1;
         body[0].x = 40;
@@ -323,51 +400,61 @@
         body.push(new rectangle(0, 0, 10, 10));
         food.x = random(buffer.width / 10 - 1) * 10;
         food.y = random(buffer.height / 10 - 1) * 10;
+        stop = false;
         gameOver = false;
         aDie.pause();
         aDie.load();
     }
 
     function init() {
-    // Load buffer
-    buffer = document.createElement('canvas');
-    bufferCtx = buffer.getContext('2d');
-    buffer.width = 300;
-    buffer.height = 150;
-
-    gameWindow = document.getElementById('gameWindow');
-    gameCtx = gameWindow.getContext('2d');
-    gameOver = false;
-    stop = true;
-
-    // body.length = 0;
-    body.push(new rectangle(40, 40, 10, 10));
-    body.push(new rectangle(0, 0, 10, 10));
-    body.push(new rectangle(0, 0, 10, 10));
-
-    iNeck.src = 'assets/neck.png';
-    iBody.src = 'assets/body.png';
-    iTale.src = 'assets/tale.png';
-    iApple.src = 'assets/apple.png';
-    aEat.src = 'assets/eat.wav';
-    aSpecial.src ='assets/special.wav';
-    aDie.src = 'assets/death.mp4';
-    aEat.volume = 0.2;
-    aDie.volume = 0.5;
-    aSpecial.volume = 0.5;
-
-    wall.push(new rectangle(100, 50, 10, 10));
-    wall.push(new rectangle(100, 100, 10, 10));
-    wall.push(new rectangle(200, 50, 10, 10));
-    wall.push(new rectangle(200, 100, 10, 10));
-
-    resize();
-    run();
-    repaint();
+        // Load buffer
+        buffer = document.createElement('canvas');
+        bufferCtx = buffer.getContext('2d');
+        buffer.width = 300;
+        buffer.height = 150;
+        //Load canvas
+        gameWindow = document.getElementById('gameWindow');
+        gameCtx = gameWindow.getContext('2d');
+        //Set start values
+        gameOver = false;
+        stop = false;
+        //Create player character
+        body.push(new rectangle(40, 40, 10, 10));
+        body.push(new rectangle(0, 0, 10, 10));
+        body.push(new rectangle(0, 0, 10, 10));
+        //Create food
+        food = new rectangle(80, 80, 10, 10);
+        //Create obstacles
+        wall.push(new rectangle(100, 50, 10, 10));
+        wall.push(new rectangle(200, 50, 10, 10));
+        wall.push(new rectangle(100, 100, 10, 10));
+        wall.push(new rectangle(110, 110, 90, 10));
+        wall.push(new rectangle(200, 100, 10, 10));
+        //Load assets
+        iNeck.src = 'assets/neck.png';
+        iBody.src = 'assets/body.png';
+        iTale.src = 'assets/tale.png';
+        iApple.src = 'assets/apple.png';
+        aEat.src = 'assets/eat.wav';
+        aSpecial.src ='assets/special.wav';
+        aDie.src = 'assets/death.mp4';
+        aEat.volume = 0.2;
+        aDie.volume = 0.5;
+        aSpecial.volume = 0.5;
+        // Load saved highscores
+        if (localStorage.highscores) {
+            highscores = localStorage.highscores.split(',');
+        }
+        //Inicialice game
+        resize();
+        run();
+        repaint();
     }
     window.addEventListener('resize', resize, false)
     window.addEventListener('load', init, false);
 
-
+    function clearScore(){
+        localStorage.highscores = undefined;
+    }
 }(window))
 
